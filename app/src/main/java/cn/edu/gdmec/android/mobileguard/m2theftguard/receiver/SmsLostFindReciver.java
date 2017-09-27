71aa113d26dd596b0e3207fa4724cab59533e902
huigesi <791339970@qq.com>
2017-09-27 23:29:09 +0800

    更新模块二(未完2)

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/receiver/SmsLostFindReciver.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/receiver/SmsLostFindReciver.java
@@ -7,6 +7,12 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.text.TextUtils;
+import android.util.Log;
+
+import cn.edu.gdmec.android.mobileguard.m2theftguard.service.GPSLocationService;
 
 /**
  * Created by 黄煜辉 on 2017/9/20.
@@ -24,7 +30,22 @@ public class SmsLostFindReciver extends BroadcastReceiver{
         if (protecting){
             Object[] objs=(Object[]) intent.getExtras().get("pdus");
             DevicePolicyManager dpm = (DevicePolicyManager)context.getSystemService(Context.DEVICE_POLICY_SERVICE);
-
+            for (Object obj : objs){
+                SmsMessage smsMessage= SmsMessage.createFromPdu((byte[]) obj);
+                String sender=smsMessage.getOriginatingAddress();
+                if(sender.startsWith("+86")){
+                    sender=sender.substring(3, sender.length());
+                }
+                String body=smsMessage.getMessageBody();
+                String safephone=sharedPreferences.getString("safephone",null);
+                if (!TextUtils.isEmpty(safephone)&sender.equals(safephone)){
+                    if (("#*location*#".equals(body))){
+                        Log.i(TAG,"返回位置信息.");
+                        Intent service=new Intent(context, GPSLocationService.class);
+                        context.startService(service);
+                    }
+                }
+           }
         }
     }
 }
