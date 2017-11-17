1bbd2e2e02f832c0bad0c66be0c05127de0cc60f
huigesi <791339970@qq.com>
2017-11-17 14:04:44 +0800

    m5重新做2

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
@@ -1,6 +1,5 @@
 package cn.edu.gdmec.android.mobileguard;
 
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
@@ -20,19 +19,12 @@ public class SplashActivity extends AppCompatActivity {
         mVersion = MyUtils.getVersion(getApplicationContext());
         mTvVersion=(TextView)findViewById(R.id.tv_splash_version);
         mTvVersion.setText("版本号"+mVersion);
-        VersionUpdateUtils.DownloadCallback downloadCallback = new VersionUpdateUtils.DownloadCallback() {
-            @Override
-            public void afterDownload(String filename) {
-
-            }
-        };
-        final VersionUpdateUtils versionUpdateUtils=new VersionUpdateUtils(mVersion,SplashActivity.this,downloadCallback,null);
-        //localDbVersion,VirusScanActivity.this,downloadCallback,null
+        final VersionUpdateUtils versionUpdateUtils=new VersionUpdateUtils(mVersion,SplashActivity.this);
         new Thread(){
             @Override
             public void run(){
                 super.run();
-                versionUpdateUtils.getCloudVersion("http://android2017.duapp.com/updateinfo.html");
+                versionUpdateUtils.getCloudVersion();
             }
         }.start();
     }
