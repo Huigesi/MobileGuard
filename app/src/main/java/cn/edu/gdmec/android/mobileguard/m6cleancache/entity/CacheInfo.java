6117db1bd0ebcf7b5b7731833a8c928e5c1cd6aa
huigesi <791339970@qq.com>
2017-11-19 12:03:34 +0800

    m6完成并且调试成功

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m6cleancache/entity/CacheInfo.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m6cleancache/entity/CacheInfo.java
@@ -1,8 +1,17 @@
 package cn.edu.gdmec.android.mobileguard.m6cleancache.entity;
 
+import android.graphics.drawable.Drawable;
+
 /**
  * Created by 黄煜辉 on 2017/9/20.
  */
 
 public class CacheInfo {
+    public String packagename;
+
+    public long cacheSize;
+
+    public Drawable appIcon;
+
+    public String appName;
 }
