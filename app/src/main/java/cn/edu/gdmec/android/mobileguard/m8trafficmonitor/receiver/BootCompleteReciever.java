c3b37685013e84803c472baf9b9305f38d97e209
shiyifei <806987963@qq.com>
2017-11-27 08:47:12 +0800

    m8代码

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m8trafficmonitor/receiver/BootCompleteReciever.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m8trafficmonitor/receiver/BootCompleteReciever.java
@@ -1,8 +1,26 @@
 package cn.edu.gdmec.android.mobileguard.m8trafficmonitor.receiver;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import cn.edu.gdmec.android.mobileguard.m8trafficmonitor.service.TrafficMonitoringService;
+import cn.edu.gdmec.android.mobileguard.m8trafficmonitor.utils.SystemInfoUtils;
+
 /**
  * Created by 黄煜辉 on 2017/9/20.
  */
 
-public class BootCompleteReciever {
+public class BootCompleteReciever{/* extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent){
+
+        if (!SystemInfoUtils.isServiceRunning(context,
+                "cn.edu.gdmec.android.mobileguard.m8trafficmonitor.service.TrafficMonitoringService")){
+
+            Log.d("traffic service","turn on");
+            context.startService(new Intent(context, TrafficMonitoringService.class));
+        }
+    }*/
 }
